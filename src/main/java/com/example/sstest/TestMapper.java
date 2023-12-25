4229ad74f08aa1842d2efda195383c949a74499f
yangdacheng <yangdacheng@xkzx.com>
2023-12-25 11:14:32 +0800

    Init

--- src/main/java/com/example/sstest/TestMapper.java
+++ src/main/java/com/example/sstest/TestMapper.java
@@ -2,12 +2,13 @@ package com.example.sstest;
 
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.Date;
 
 @Mapper
 public interface TestMapper {
 
-    @Insert("INSERT INTO nx_data ( id, point_guid, tag, `value`, `timestamp` ) VALUES ( ?, ?, ?, ?, ? )")
-    void insert(Long id, String pointGuid, String tag, String value, Date timestamp);
+    @Insert("INSERT INTO nx_data ( id, point_guid, tag, `value`, `timestamp` ) VALUES ( #{id}, #{pointGuid} , #{tag} , #{value} , #{timestamp}  )")
+    void insert(@Param("id") Long id, @Param("pointGuid") String pointGuid, @Param("tag") String tag, @Param("value") String value, @Param("timestamp") Date timestamp);
 }
